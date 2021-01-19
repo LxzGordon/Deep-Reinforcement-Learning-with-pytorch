bf5709efd8f66b917a0d38ca6f0fe0325cdeab8b
LxzGordon <34240890+LxzGordon@users.noreply.github.com>
2021-01-19 17:15:53 +0800

    Update Sarsa.py

--- algorithm/value-based/Sarsa.py
+++ algorithm/value-based/Sarsa.py
@@ -1,12 +1,11 @@
+import numpy as np
 import gym
 import torch as th
-import numpy as np 
 
-batch_size=50
-lr=0.001
-episilon=0.9
-replay_memory_size=10000
+env=gym.make('CartPole-v0')
 gamma=0.9
+episilon=0.9
+lr=0.001
 target_update_iter=100
 log_internval=10
 env=gym.make('CartPole-v0')
@@ -20,9 +19,7 @@ class net(th.nn.Module):
     def __init__(self):
         super(net,self).__init__()
         self.fc1=th.nn.Linear(n_state,hidden)
-        self.fc1.weight.data.normal_(0, 0.1)
         self.out=th.nn.Linear(hidden,n_action)
-        self.out.weight.data.normal_(0, 0.1)
     
 
     def forward(self,x):
@@ -31,89 +28,55 @@ class net(th.nn.Module):
         out=self.out(x)
         return out
 
-class replay_memory():
+class Sarsa():
     def __init__(self):
-        self.memory_size=replay_memory_size
-        self.memory=np.array([])
-        self.cur=0
-        self.new=0
-    def size(self):
-        return self.memory.shape[0]
-#[s,a,r,s_,done]
-    def store_transition(self,trans):
-        if(self.memory.shape[0]<self.memory_size):
-            if self.new==0:
-                self.memory=np.array(trans)
-                self.new=1
-            elif self.memory.shape[0]>0:
-                self.memory=np.vstack((self.memory,trans))
-
-        else:
-            self.memory[self.cur,:]=trans
-            self.cur=(self.cur+1)%self.memory_size
-    
-    def sample(self):
-        if self.memory.shape[0]<batch_size:
-            return -1
-        sam=np.random.choice(self.memory.shape[0],batch_size)
-        return self.memory[sam]
-    
-class DQN(object):
-    def __init__(self):
-        self.eval_q_net,self.target_q_net=net().to(device),net().to(device)
-        self.replay_mem=replay_memory()
+        self.net,self.target_net=net(),net()
         self.iter_num=0
-        self.optimizer=th.optim.Adam(self.eval_q_net.parameters(),lr=lr)
-        self.loss=th.nn.MSELoss().to(device)
-    def choose_action(self,qs):
-        if np.random.uniform()<episilon:
-            return th.argmax(qs).tolist()
+        self.optimizer=th.optim.Adam(self.net.parameters(),lr=lr)
+
+    def learn(self,s,a,s_,r,done):
+        eval_q=self.net(th.Tensor(s))[a]
+        target_q=self.target_net(th.FloatTensor(s_))
+        target_a=self.choose_action(target_q)
+        target_q=target_q[target_a]
+        if not done:
+            y=gamma*target_q+r
         else:
-            return np.random.randint(0,n_action)
+            y=r
+        loss=(y-eval_q)**2
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.iter_num+=1
+        if self.iter_num%10==0:
+            self.target_net.load_state_dict(self.net.state_dict())
+        return target_a
+
     def greedy_action(self,qs):
         return th.argmax(qs)
-    def learn(self):
-        if(self.iter_num%target_update_iter==0):
-            self.target_q_net.load_state_dict(self.eval_q_net.state_dict())
-        self.iter_num+=1
 
-        batch=self.replay_mem.sample()
-        b_s=th.FloatTensor(batch[:,0].tolist()).to(device)
-        b_a=th.LongTensor(batch[:,1].astype(int).tolist()).to(device)
-        b_r=th.FloatTensor(batch[:,2].tolist()).to(device)
-        b_s_=th.FloatTensor(batch[:,3].tolist()).to(device)
-        b_d=th.FloatTensor(batch[:,4].tolist()).to(device)
-        q_target=th.zeros((batch_size,1)).to(device)
-        q_eval=self.eval_q_net(b_s)
-        q_eval=th.gather(q_eval,dim=1,index=th.unsqueeze(b_a,1))
-        q_next=self.target_q_net(b_s_).detach()
-        for i in range(b_d.shape[0]):
-            if(int(b_d[i].tolist()[0])==0):
-                q_target[i]=b_r[i]+gamma*th.unsqueeze(th.max(q_next[i],0)[0],0)
-            else:
-                q_target[i]=b_r[i]
-        td_error=self.loss(q_eval,q_target)
+    def random_action(self):
+        return np.random.randint(0,n_action)
 
-        self.optimizer.zero_grad()
-        td_error.backward()
-        self.optimizer.step()
+    def choose_action(self,qs):
+        if np.random.rand()>episilon:
+            return self.random_action()
+        else:
+            return self.greedy_action(qs).tolist()
 
-dqn=DQN()
-
+sarsa=Sarsa()
 for episode in range(10000):
     s=env.reset()
     t=0
     r=0.0
+    qs=sarsa.net(th.Tensor(s))
+    a=sarsa.choose_action(qs)
     while(t<300):
         t+=1
-        qs=dqn.eval_q_net(th.FloatTensor(s).to(device))
-        a=dqn.choose_action(qs)
+        #print(a)
         s_,r,done,_=env.step(a)
-        transition=[s.tolist(),a,[r],s_.tolist(),[done]]
-        dqn.replay_mem.store_transition(transition)
+        a=sarsa.learn(s,a,s_,r,done)
         s=s_
-        if dqn.replay_mem.size()>batch_size:
-            dqn.learn()
         if done:
             break
     if episode%log_internval==0: #test
@@ -125,9 +88,9 @@ for episode in range(10000):
             time=0
             while(time<300):
                 time+=1
-                t_qs=dqn.eval_q_net(th.FloatTensor(t_s).to(device))
-                t_a=dqn.greedy_action(t_qs).item()
-                ts_,tr,tdone,_=env.step(t_a)
+                qs=sarsa.net(th.Tensor(t_s))
+                a=sarsa.greedy_action(qs)
+                ts_,tr,tdone,_=env.step(a.tolist())
                 t_r+=tr
                 if tdone:
                     break
@@ -136,5 +99,7 @@ for episode in range(10000):
         print("episode:"+format(episode)+",test score:"+format(total_reward/10))
 
 
+
+        
 
 
